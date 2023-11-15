c5ca09911f38b38e2633f73597051c7579cde76d
Kerry Guarino <kerry.guarino@gmail.com>
2023-11-14 19:55:50 -0500

    added classmethod to return customer object

--- lib/models/customer.py
+++ lib/models/customer.py
@@ -125,3 +125,17 @@ class Customer:
       rows = CURSOR.execute(sql).fetchall()
 
       return [cls.instance_from_db(row) for row in rows]
+
+   @classmethod
+   def find_by_id(cls, id):
+      """Return Customer object corresponding to the table row matching the specified primary key"""
+      sql = """
+         SELECT *
+         FROM customers
+         WHERE id = ?
+      """
+
+      row = CURSOR.execute(sql, (id,)).fetchone()
+      return cls.instance_from_db(row) if row else None   
+   
+   
