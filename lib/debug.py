1a1805be98522fd6192c0e9f1e67ec75da1c9c40
Kerry Guarino <kerry.guarino@gmail.com>
2023-11-16 18:47:48 -0500

    added to debug

--- lib/debug.py
+++ lib/debug.py
@@ -2,7 +2,28 @@
 # lib/debug.py
 
 from models.__init__ import CONN, CURSOR
+from models.book import Book
+from models.customer import Customer
 import ipdb
 
 
+def reset_database():
+   Customer.drop_table()
+   Book.drop_table()
+   Book.create_table()
+   Customer.create_table()
+
+   # Create seed data
+
+   #  what is customer_id used for?
+   nyc = Book.create("Strand", "New York")
+   philly = Book.create("Blue Marble", "Philadelphia")
+   Customer.create("John", 25, nyc.id)
+   Customer.create("Dave", 38, nyc.id)
+   Customer.create("Mary", 19, philly.id)
+   Customer.create("Alex", 27, philly.id)
+   Customer.create("Beth", 42, philly.id)
+
+
+reset_database()
 ipdb.set_trace()
